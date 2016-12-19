a8193a971273f73d9f35934558be2ab8df054b89
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2016-12-18 21:35:53 -0500

    Move send-message program to bin directory.

--- examples/rust/src/bin/send-message.rs
+++ examples/rust/src/bin/send-message.rs
@@ -1,12 +1,8 @@
 extern crate lcm;
 
-// mod exlcm;
 mod exlcm {
     include!(concat!(env!("OUT_DIR"), "/exlcm/example_t.rs"));
 }
-
-// mod exlcm_backup;
-// use exlcm_backup as exlcm;
 
 fn main() {
     let mut lcm = lcm::Lcm::new();
